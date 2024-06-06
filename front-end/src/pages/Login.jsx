0c1f818dad7fca43c073f8c8540f096aeb87f91a
Srijan Agrawal <asrijan889@gmail.com>
2024-06-06 16:41:09 +0530

    some minor changes

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,11 +1,12 @@
-import React, { useState , useContext } from 'react'
+import React, { useState, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { FcGoogle } from "react-icons/fc";
 import { UserContext } from '../components/Context/UserContext';
 import toast from 'react-hot-toast';
+import spaghettiImage from '../assets/spaghetti.jpg'; // Adjust the path based on your project structure
 
 function Login() {
-  const {loggedIn } = useContext(UserContext);
+  const { loggedIn } = useContext(UserContext);
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -19,61 +20,62 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-        const response = await fetch("http://localhost:3000/user/login", {
-            method: 'POST',
-            body: JSON.stringify(formData),
-            headers: { 'Content-Type': 'application/json' },
-            credentials: 'include',
-        });
-        const data = await response.json();
+      const response = await fetch("http://localhost:3000/user/login", {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
+      });
+      const data = await response.json();
 
-        if (response.ok) {
-          loggedIn(data.token, data.email, data.firstName, data.lastName , data.id, data.favorites);
-          navigate("/");
-          toast.success("Login Successful");
-          
-        }
-        if (data.success !== true) {
-        }
+      if (response.ok) {
+        loggedIn(data.token, data.email, data.firstName, data.lastName, data.id, data.favorites);
+        navigate("/");
+        toast.success("Login Successful");
+      } else {
+        toast.error(data.message || "Login Failed");
+      }
     } catch (error) {
-        console.error('Error:', error);
-        toast.error(error.message);
+      console.error('Error:', error);
+      toast.error(error.message);
     }
-};
+  };
   
   return (
     <div className="w-full h-screen flex font-['Founders_Grotesk_X_Condensed'] text-[#F8FAE5]">
-        <div className="relative image w-1/2 flex flex-col">
-            <img src="./src/assets/spaghetti.jpg" alt="food" className="absolute bg-cover w-full object-cover h-screen z-1 opacity-80 brightness-90"/>
-            <div className="flex items-center justify-center w-full h-full">
-            <div className="uppercase absolute top-40 text-[13vw] font-light "> New User?</div>
-            <Link to="/signup" className="absolute flex items-center justify-center pt-24">
-            <button className=" border rounded-2xl bottom-60 px-14 pb-6 text-4xl pt-6 leading-[.6] bg-[#43766C] uppercase tracking-tight font-['Neue_Montreal']">Signup</button> </Link>
-        </div></div>
-        <div className="flex w-full lg:w-1/2 justify-center items-center bg-[#5e968b] space-y-8 font-['Neue_Montreal']">
-            <div className="w-full px-8 md:px-32 lg:px-24">
-            <form className="bg-[#F8FAE5] rounded-md shadow-4xl p-5" onSubmit={handleSubmit}>
-              <h1 className="text-[#43766C] font-[600] text-4xl mb-3">Hello There!</h1>
-              <p className="text-2xl font-light text-[#43766C] mb-8">Welcome Back</p>
-              <div className="flex items-center border-2 mb-8 py-2 px-3 rounded-2xl">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
-                </svg>
-                <input id="email" type="email" name="email" className="bg-[#F8FAE5] text-[#43766C] text-xl pl-2 w-full focus:ring-[#F8FAE5] border-none" onChange={handleChange} placeholder="Email Address" />
-              </div>
-              <div className="flex items-center border-2 mb-12 py-2 px-3 rounded-2xl ">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                  <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
-                </svg>
-                <input className="pl-2 w-full border-none bg-[#F8FAE5] text-[#43766C] focus:ring-[#F8FAE5] text-xl" type="password" name="password" id="password" onChange={handleChange} placeholder="Password" />
-              </div>
-              <button type="submit" className="block w-full bg-[#43766C] my-5 py-2 rounded-2xl hover:bg-[#39665d] hover:-translate-y-1 transition-all duration-500 text-white font-semibold text-2xl ">Login</button>
-              {/* <button type="submit"  className="flex justify-center w-full bg-[#43766C] my-5 py-2 rounded-2xl hover:bg-[#39665d] hover:-translate-y-1 transition-all duration-500 text-white font-semibold text-2xl " onClick={loginwithgoogle} > <FcGoogle size={30} className="mr-4"  /> Login with google </button> */}
-              </form>
+      <div className="relative w-1/2 hidden lg:flex flex-col">
+        <img src={spaghettiImage} alt="food" className="absolute bg-cover w-full object-cover h-screen z-1 opacity-80 brightness-90" />
+        <div className="flex items-center justify-center w-full h-full">
+          <div className="uppercase absolute top-40 text-[13vw] font-light">New User?</div>
+          <Link to="/signup" className="absolute flex items-center justify-center pt-24">
+            <button className="border rounded-2xl bottom-60 px-14 pb-6 text-4xl pt-6 leading-[.6] bg-[#43766C] uppercase tracking-tight font-['Neue_Montreal']">Signup</button>
+          </Link>
+        </div>
+      </div>
+      <div className="flex w-full lg:w-1/2 justify-center items-center bg-[#5e968b] space-y-8 font-['Neue_Montreal']">
+        <div className="w-full px-8 md:px-32 lg:px-24">
+          <form className="bg-[#F8FAE5] rounded-md shadow-4xl p-5" onSubmit={handleSubmit}>
+            <h1 className="text-[#43766C] font-[600] text-4xl mb-3">Hello There!</h1>
+            <p className="text-2xl font-light text-[#43766C] mb-8">Welcome Back</p>
+            <div className="flex items-center border-2 mb-8 py-2 px-3 rounded-2xl">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+              </svg>
+              <input id="email" type="email" name="email" className="bg-[#F8FAE5] text-[#43766C] text-xl pl-2 w-full focus:ring-[#F8FAE5] border-none" onChange={handleChange} placeholder="Email Address" />
             </div>
+            <div className="flex items-center border-2 mb-12 py-2 px-3 rounded-2xl">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+              </svg>
+              <input className="pl-2 w-full border-none bg-[#F8FAE5] text-[#43766C] focus:ring-[#F8FAE5] text-xl" type="password" name="password" id="password" onChange={handleChange} placeholder="Password" />
+            </div>
+            <button type="submit" className="block w-full bg-[#43766C] my-5 py-2 rounded-2xl hover:bg-[#39665d] hover:-translate-y-1 transition-all duration-500 text-white font-semibold text-2xl">Login</button>
+            {/* <button type="submit" className="flex justify-center w-full bg-[#43766C] my-5 py-2 rounded-2xl hover:bg-[#39665d] hover:-translate-y-1 transition-all duration-500 text-white font-semibold text-2xl" onClick={loginwithgoogle}><FcGoogle size={30} className="mr-4" /> Login with google </button> */}
+          </form>
+        </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,17 +1,39 @@
-import { defineConfig, loadEnv} from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-// https://vitejs.dev/config/
+// Load environment variables
+dotenv.config()
+
 export default defineConfig({
   server: {
     proxy: {
-      '/api':{
-        target:'http://localhost:3000',
-        secure:false,
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
       },
     },
   },
-  plugins: [react()],
-  define: {'process.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY)}
+  plugins: [
+    react(),
+    visualizer({
+      filename: './dist/stats.html',
+      open: true,
+    }),
+  ],
+  define: {
+    'process.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY)
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Split vendor libraries into a separate chunk
+          // Add more chunks as needed
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Set the limit to 1000 kB or any size you consider appropriate
+  },
 })

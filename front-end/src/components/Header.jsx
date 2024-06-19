9a0b6ad2ccb2f627717e6a89a6ad3cb83fb1529d
Srijan Agrawal <asrijan889@gmail.com>
2024-06-19 21:17:54 +0530

    responsiveness added

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,30 +1,32 @@
-import React, { useContext } from "react";
-import { useState, useEffect } from 'react';
-import {Link, NavLink} from "react-router-dom";
-import Logo from '../assets/logo.png'
+import React, { useContext, useState, useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
+import Logo from '../assets/logo.png';
 import { UserContext } from "./Context/UserContext";
 import toast from "react-hot-toast";
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Header = () => {
   const [isHidden, setIsHidden] = useState(false);
   const [prevScrollPos, setPrevScrollPos] = useState(window.scrollY);
-  const {isLoggedIn, setIsLoggedIn, loggedout} = useContext(UserContext);
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // Initialize isMenuOpen to false
+  const { isLoggedIn, setIsLoggedIn, loggedout } = useContext(UserContext);
+
   const links = [
-    {name: "Home", path: "/"},
-    {name: "Post Recipes", path: "/user/postrecipe"},
-    {name: "Community", path: "/user/viewrecipe"},
-    {name: "Recipe Books", path: "/community/user/books"},
-  ]
+    { name: "Home", path: "/" },
+    { name: "Post Recipes", path: "/user/postrecipe" },
+    { name: "Community", path: "/user/viewrecipe" },
+    { name: "Recipe Books", path: "/community/user/books" },
+  ];
 
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollPos = window.scrollY;
-      setIsHidden(prevScrollPos <  currentScrollPos && currentScrollPos > 100);
+      setIsHidden(prevScrollPos < currentScrollPos && currentScrollPos > 100);
       setPrevScrollPos(currentScrollPos);
     };
 
     window.addEventListener('scroll', handleScroll);
-    
+
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
@@ -34,34 +36,112 @@ const Header = () => {
     loggedout();
     setIsLoggedIn(false);
     toast.success("Logged out successfully");
-  }
+  };
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
 
   return (
-    <nav style={{ display: isHidden ? 'none' : 'block'  }}>
-    <div  className="fixed z-[5] w-full px-12 flex justify-between items-center backdrop-blur-[3px] text-[#F8FAE5]">
-      <div className="logo flex gap-5">
-       <img src={Logo} alt="logo"  className="bg-transparent w-20 py-3"/>
-       {/* <img src="./src/assets/recipe-rover.png" alt="logo" className="bg-transparent object-scale-down w-48" /> */}
-      </div>
-      <div className="links flex gap-5 py-10 ml-56">
-        {links.map(({name, path}, index) => (
-          <NavLink to={path} key={index} className={({isActive}) => `text-lg font-light font-['Neue_Montreal'] ${isActive? "underline" : ""} `}>{name}</NavLink>
-        ))}
+    <>
+      {/* Default Navbar */}
+      <nav style={{ display: isHidden ? 'none' : 'block' }}>
+        <div className="fixed z-[5] w-full px-12 flex justify-between items-center backdrop-blur-[3px] text-[#F8FAE5]">
+          <div className="logo flex gap-5">
+            <img src={Logo} alt="logo" className="bg-transparent w-20 py-3" />
+          </div>
+          <div className="hidden md:flex links gap-5 py-10 ml-56">
+            {links.map(({ name, path }, index) => (
+              <NavLink to={path} key={index} className={({ isActive }) => `text-lg font-light font-['Neue_Montreal'] ${isActive ? "underline" : ""}`}>
+                {name}
+              </NavLink>
+            ))}
+          </div>
+          {isLoggedIn ? (
+            <Link to="/" onClick={clickHandler} className="hidden md:block">
+              <div className="button -mt-2 border rounded-xl p-4 uppercase text-sm leading-[.6] bg-[#43766C]"> Logout</div>
+            </Link>
+          ) : (
+            <Link to="/login" className="hidden md:block">
+              <div className="button -mt-2 border rounded-xl p-4 uppercase text-sm leading-[.6] bg-[#43766C]"> Login/ Signup</div>
+            </Link>
+          )}
+          <div className="md:hidden flex items-center">
+            <button onClick={toggleMenu} className="text-3xl">
+              {isMenuOpen ? <FaTimes /> : <FaBars />}
+            </button>
+          </div>
+        </div>
+      </nav>
 
-      </div>
-          {isLoggedIn?(
-            <Link to="/" onClick={clickHandler}>
-            <div className="button -mt-2 border rounded-xl p-4 uppercase text-sm leading-[.6] bg-[#43766C]"> Logout</div></Link>
-              ):(
-      <Link to="/login">
-      <div className="button -mt-2 border rounded-xl p-4 uppercase text-sm leading-[.6] bg-[#43766C]"> Login/ Signup</div></Link>
-          )
-      }   
-    </div>
-    </nav>
+      {/* Fullscreen Navbar */}
+      {isMenuOpen && (
+        <div className={`fixed top-0 left-0 w-full h-full bg-[#43766C] flex flex-col items-center justify-center z-10 transition-transform duration-300 ease-in-out ${isMenuOpen ? 'menu-open' : 'menu-close'}`}>
+          <button onClick={closeMenu} className="absolute top-5 right-5 text-3xl text-[#F8FAE5]">
+            <FaTimes />
+          </button>
+          {links.map(({ name, path }, index) => (
+            <NavLink
+              to={path}
+              key={index}
+              className="text-2xl font-light font-['Neue_Montreal'] mb-4"
+              onClick={closeMenu}
+            >
+              {name}
+            </NavLink>
+          ))}
+          {isLoggedIn ? (
+            <Link to="/" onClick={() => { clickHandler(); closeMenu(); }}>
+              <div className="button mt-4 border rounded-xl p-4 uppercase text-lg leading-[.6] bg-[#F8FAE5] text-[#43766C]"> Logout</div>
+            </Link>
+          ) : (
+            <Link to="/login" onClick={closeMenu}>
+              <div className="button mt-4 border rounded-xl p-4 uppercase text-lg leading-[.6] bg-[#F8FAE5] text-[#43766C]"> Login/ Signup</div>
+            </Link>
+          )}
+        </div>
+      )}
+
+      {/* Styles for Fullscreen Navbar Animation */}
+      <style jsx="true">
+        {`
+          .menu-open {
+            animation: fadeInSlideIn 0.5s forwards;
+          }
+
+          .menu-close {
+            animation: fadeOutSlideOut 0.5s forwards;
+          }
+
+          @keyframes fadeInSlideIn {
+            0% {
+              opacity: 0;
+              transform: translateX(100%);
+            }
+            100% {
+              opacity: 1;
+              transform: translateX(0);
+            }
+          }
+
+          @keyframes fadeOutSlideOut {
+            0% {
+              opacity: 1;
+              transform: translateX(0);
+            }
+            100% {
+              opacity: 0;
+              transform: translateX(100%);
+            }
+          }
+        `}
+      </style>
+    </>
   );
-}
-
+};
 
 export default Header;
--- front-end/src/pages/UserRecipe.jsx
+++ front-end/src/pages/UserRecipe.jsx
@@ -8,19 +8,18 @@ import toast from "react-hot-toast";
 
 function UserRecipe() {
   const navigate = useNavigate();
-  const { id } = useParams(); //id of recipe
-  const [details, setDetails] = useState({}); // to show recipe details
+  const { id } = useParams();
+  const [details, setDetails] = useState({}); 
   const apiURL = import.meta.env.VITE_SERVER_URL;
 
-
-  const [comment, setComment] = useState(""); //for text of comment
-  const [comments, setComments] = useState([]); // to show all comments
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]); 
 
   const [commentBox, setCommentBox] = useState(false);
   const [likeCount, setLikeCount] = useState(0);
   const [commentCount, setCommentCount] = useState(0);
 
-  const postedBy = details && details.postedBy
+  const postedBy = details && details.postedBy;
 
   const userInfo = localStorage.getItem("token")
     ? {
@@ -31,9 +30,7 @@ function UserRecipe() {
       }
     : null;
 
-    
-  const userID = userInfo &&userInfo.userId
-
+  const userID = userInfo && userInfo.userId;
 
   const fetchData = async () => {
     const response = await fetch(`${apiURL}/recipe/get/${id}`);
@@ -48,23 +45,19 @@ function UserRecipe() {
     fetchData();
   }, []);
 
-
   const deleteRecipe = async () => {
     try {
-      const response = await fetch(
-        `${apiURL}/recipe/delete/${id}`,
-        {
-          method: "DELETE",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${userInfo.token}`,
-          },
-        }
-      );
+      const response = await fetch(`${apiURL}/recipe/delete/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      });
       const data = await response.json();
       if (data.success === true) {
-        toast('Recipe Deleted.', {
-          icon: '🗑️',
+        toast("Recipe Deleted.", {
+          icon: "🗑️",
         });
         navigate("/user/viewrecipe");
       }
@@ -76,25 +69,22 @@ function UserRecipe() {
   const addComment = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch(
-        `${apiURL}/recipe/comment/${id}`,
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${userInfo.token}`,
-          },
-          body: JSON.stringify({
-            id,
-            comment: comment,
-            name: userInfo.name,
-          }),
-        }
-      );
+      const response = await fetch(`${apiURL}/recipe/comment/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+        body: JSON.stringify({
+          id,
+          comment: comment,
+          name: userInfo.name,
+        }),
+      });
       const data = await response.json();
       if (data.success === true) {
-        toast('Comment added Successfully', {
-          icon: '💬',
+        toast("Comment added Successfully", {
+          icon: "💬",
         });
         fetchData();
         setComment("");
@@ -105,50 +95,39 @@ function UserRecipe() {
   };
 
   const showCommentBox = () => {
-    if (commentBox) {
-      setCommentBox(false);
-    } else {
-      setCommentBox(true);
-    }
+    setCommentBox((prev) => !prev);
   };
 
   const addLike = async () => {
     try {
-      const { data } = await fetch(
-        `${apiURL}/recipe/addLike/${id}`,
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${userInfo.token}`,
-          },
-          body: JSON.stringify({
-            id,
-          }),
-        }
-      );
+      const { data } = await fetch(`${apiURL}/recipe/addLike/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+        body: JSON.stringify({
+          id,
+        }),
+      });
       fetchData();
     } catch (error) {
       console.log(error);
     }
   };
 
-  //remove like
   const removeLike = async () => {
     try {
-      const { data } = await fetch(
-        `${apiURL}/recipe/removeLike/${id}`,
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${userInfo.token}`,
-          },
-          body: JSON.stringify({
-            id,
-          }),
-        }
-      );
+      const { data } = await fetch(`${apiURL}/recipe/removeLike/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+        body: JSON.stringify({
+          id,
+        }),
+      });
       fetchData();
     } catch (error) {
       console.log(error.response.data.error);
@@ -157,23 +136,20 @@ function UserRecipe() {
 
   const addFavorite = async () => {
     try {
-      const response = await fetch(
-        `${apiURL}/recipe/addFavorite/${id}`,
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${userInfo.token}`,
-          },
-          body: JSON.stringify({
-            id,
-          }),
-        }
-      );
+      const response = await fetch(`${apiURL}/recipe/addFavorite/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+        body: JSON.stringify({
+          id,
+        }),
+      });
       const data = await response.json();
       localStorage.setItem("favorites", data);
-      toast('Added to favorites', {
-        icon: '❤️',
+      toast("Added to favorites", {
+        icon: "❤️",
       });
       fetchData();
     } catch (error) {
@@ -183,23 +159,20 @@ function UserRecipe() {
 
   const removeFavorite = async () => {
     try {
-      const response = await fetch(
-        `${apiURL}/recipe/removeFavorite/${id}`,
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${userInfo.token}`,
-          },
-          body: JSON.stringify({
-            id,
-          }),
-        }
-      );
+      const response = await fetch(`${apiURL}/recipe/removeFavorite/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+        body: JSON.stringify({
+          id,
+        }),
+      });
       const data = await response.json();
       localStorage.setItem("favorites", data);
-      toast('Removed from favorites', {
-        icon: '❤️',
+      toast("Removed from favorites", {
+        icon: "❤️",
       });
       fetchData();
     } catch (error) {
@@ -207,27 +180,23 @@ function UserRecipe() {
     }
   };
 
+  const [isFavorite, setIsFavorite] = useState(false);
 
- 
-const [isFavorite, setIsFavorite] = useState(false);
-
-  // useEffect to update isFavorite when userInfo or id changes
   useEffect(() => {
-    // Check if the recipe ID of this page is in the user's favorites
-    if(userInfo !== null){
+    if (userInfo !== null) {
       setIsFavorite(userInfo.favorites.includes(id));
     }
-  }, [userInfo, id]); // Update isFavorite when userInfo or id changes
+  }, [userInfo, id]);
 
   const likesId = details && details.likes;
   return (
     <div className="w-full flex flex-col bg-gradient-to-br from-slate-500 to-slate-900 font-['Founders_Grotesk_X_Condensed'] text-[#F8FAE5] items-center">
-      <div className="relative my-20 mx-auto w-[80vw] bg-transparent rounded-2xl backdrop-brightness-75 backdrop-blur-sm shadow-2xl">
+      <div className="relative my-20 mx-auto w-[90vw] lg:w-[70vw] bg-transparent rounded-2xl backdrop-brightness-75 backdrop-blur-sm shadow-2xl">
         <div className="flex flex-col">
-          <h1 className="uppercase text-[15vh] flex items-end justify-center text-center pt-[7vh] font-light leading-none">
+          <h1 className="uppercase text-4xl md:text-5xl lg:text-6xl xl:text-7xl flex items-end justify-center text-center pt-10 md:pt-14 font-light leading-none">
             {details.title}
           </h1>
-          <div className="mx-32 h-[24rem] w-auto object-cover rounded-xl overflow-hidden flex items-center justify-center">
+          <div className="mx-5 md:mx-20 lg:mx-32 h-60 md:h-80 lg:h-96 w-auto object-cover rounded-xl overflow-hidden flex items-center justify-center">
             <img
               src={details.image && details.image.url}
               alt={details.title}
@@ -235,56 +204,50 @@ const [isFavorite, setIsFavorite] = useState(false);
             />
           </div>
           {postedBy === userID && (
-              <div className="flex items-center justify-center gap-10 my-5 font-['Neue_Montreal'] text-[1vw]">
-                <Link to={`/user/recipe/update/${id}`}>
-                  <button className="rounded-xl p-4 uppercase bg-[#00a86b] hover:bg-[#00a86aba] text-white font-bold">
-                    Update recipe
-                  </button>
-                </Link>
-                <button
-                  onClick={deleteRecipe}
-                  className="rounded-xl p-4 uppercase bg-[#d2042d] hover:bg-[#d2042dc1] text-white font-bold"
-                >
-                  {" "}
-                  Delete Recipe
+            <div className="flex items-center justify-center gap-5 md:gap-10 my-5 font-['Neue_Montreal'] text-base md:text-lg lg:text-xl">
+              <Link to={`/user/recipe/update/${id}`}>
+                <button className="rounded-xl p-2 md:p-3 lg:p-4 uppercase bg-[#00a86b] hover:bg-[#00a86aba] text-white font-bold">
+                  Update recipe
                 </button>
-              </div>
-            )}
+              </Link>
+              <button
+                onClick={deleteRecipe}
+                className="rounded-xl p-2 md:p-3 lg:p-4 uppercase bg-[#d2042d] hover:bg-[#d2042dc1] text-white font-bold"
+              >
+                Delete Recipe
+              </button>
+            </div>
+          )}
 
-            <div className="flex items-center justify-between gap-10 my-5 mx-36 font-['Neue_Montreal'] text-[1vw]">
+          <div className="flex flex-col md:flex-row items-center justify-between gap-5 md:gap-10 my-5 mx-5 md:mx-20 lg:mx-36 font-['Neue_Montreal'] text-sm md:text-base lg:text-lg">
+            <span>{new Date(details.createdAt).toLocaleDateString()}</span>
+            <span className="font-bold">
+              - Posted By {details && details.authorName}
+            </span>
             <span>
-            {new Date(details.createdAt).toLocaleDateString()}
+              - {likeCount} Likes ❤️ &nbsp; {commentCount} Comments 💬
             </span>
-            <span className=" font-bold ">
-              -Posted By {details && details.authorName}
-            </span>
-        <span>
-          - {likeCount} Likes❤️ & &nbsp;
-           {commentCount} Comments 💬
-        </span>
-              </div>
+          </div>
 
-          <div className="flex mb-10 font-['Neue_Montreal'] text-[3vh]">
-            <div className="w-[35%] ml-20 ingredients">
-              <h1 className="uppercase pt-[7vh] text-[8vh] font-['Founders_Grotesk_X_Condensed'] font-light leading-none">
-                ingredients
+          <div className="flex flex-col md:flex-row mb-10 px-7 md:px-0Y font-['Neue_Montreal'] text-lg lg:text-xl">
+            <div className="w-full md:w-[35%] ml-5 md:ml-10 lg:ml-20">
+              <h1 className="uppercase pt-10 md:pt-14 text-2xl lg:text-3xl xl:text-4xl font-light font-['Founders_Grotesk_X_Condensed'] leading-tight">
+                Ingredients
               </h1>
               <div className="data-display">
                 <h2 dangerouslySetInnerHTML={{ __html: details.ingredients }} />
               </div>
             </div>
-            <div className="w-[60%] mx-16 instructions">
-              <h1 className="uppercase pt-[7vh] text-[8vh] font-['Founders_Grotesk_X_Condensed'] font-light leading-none">
-                recipe instructions
+            <div className="w-full md:w-[60%] mx-5 md:mx-10 lg:mx-16">
+              <h1 className="uppercase pt-10 md:pt-14 text-2xl lg:text-3xl xl:text-4xl font-light font-['Founders_Grotesk_X_Condensed'] leading-tight">
+                Recipe Instructions
               </h1>
               <div>
-                {" "}
-                Summary:{details.summary} <br />
+                Summary: {details.summary}
+                <br />
                 <br />
                 <div className="data-display">
-                  <h2
-                    dangerouslySetInnerHTML={{ __html: details.instructions }}
-                  />
+                  <h2 dangerouslySetInnerHTML={{ __html: details.instructions }} />
                 </div>
               </div>
             </div>
@@ -303,40 +266,40 @@ const [isFavorite, setIsFavorite] = useState(false);
       </div>
 
       {userInfo !== null ? (
-      <div className=" relative mb-10 mx-auto bg-transparent rounded-2xl backdrop-brightness-75 backdrop-blur-sm shadow-2xl flex gap-8 p-5 text-3xl ">
-        {likesId?.includes(userInfo && userInfo.userId) ? (
-          <button onClick={removeLike}>
-            <MdFavorite size={50} />
-            Like
+        <div className="relative mb-10 mx-auto bg-transparent rounded-2xl backdrop-brightness-75 backdrop-blur-sm shadow-2xl flex flex-col md:flex-row gap-4 md:gap-8 p-5 text-2xl md:text-3xl">
+          {likesId?.includes(userInfo && userInfo.userId) ? (
+            <button onClick={removeLike} className="mx-auto">
+              <MdFavorite size={50} className="mx-auto my-[.4rem]" />
+              Like
+            </button>
+          ) : (
+            <button onClick={addLike} className="mx-auto">
+              <MdFavoriteBorder size={50} className="mx-auto my-[.4rem]" />
+              Like
+            </button>
+          )}
+          <button onClick={showCommentBox} className="text-center ml-3">
+            <FaCommentAlt size={40} className="mx-auto my-[.4rem]" />
+            <span className="pt-10">Comment</span>
           </button>
-        ) : (
-          <button onClick={addLike}>
-            <MdFavoriteBorder size={50} />
-            Like
-          </button>
-        )}
-        <button onClick={showCommentBox} className="text-center ml-3">
-          <FaCommentAlt size={40} className="mx-auto my-[.4rem]" />{" "}
-          <span className="pt-10">Comment</span>
-        </button>
-        {isFavorite ? (
-          <button onClick={removeFavorite}>
-            <FaBookmark size={40} className="mx-auto my-[.4rem]" />
-            <span className="pt-10">Remove from Favorite</span>
-          </button>
-        ) : (
-          <button onClick={addFavorite}>
-            <FaRegBookmark size={40} className="mx-auto my-[.4rem]" />
-            <span className="pt-10">Save as Favorite</span>
-          </button>
-        )}
-      </div>
+          {isFavorite ? (
+            <button onClick={removeFavorite}>
+              <FaBookmark size={40} className="mx-auto my-[.4rem]" />
+              <span className="pt-10">Remove from Favorite</span>
+            </button>
+          ) : (
+            <button onClick={addFavorite}>
+              <FaRegBookmark size={40} className="mx-auto my-[.4rem]" />
+              <span className="pt-10">Save as Favorite</span>
+            </button>
+          )}
+        </div>
       ) : (
         ""
       )}
 
       {userInfo !== null && commentBox ? (
-        <div className="flex justify-around items-start py-4 pb-10 gap-20">
+        <div className="flex flex-col md:flex-row justify-around items-start py-4 pb-10 gap-5 md:gap-20">
           <div>
             {comments.length === 0 ? (
               ""
@@ -345,11 +308,7 @@ const [isFavorite, setIsFavorite] = useState(false);
                 <h5 className="pt-3 mb-2 font-['Neue_Montreal']">Comments:</h5>
 
                 {comments.map((comment) => (
-                  <CommentList
-                    key={comment._id}
-                    name={comment.postedBy.name}
-                    text={comment.text}
-                  />
+                  <CommentList key={comment._id} name={comment.postedBy.name} text={comment.text} />
                 ))}
               </>
             )}
@@ -363,13 +322,10 @@ const [isFavorite, setIsFavorite] = useState(false);
                 aria-label="minimum height"
                 rows={3}
                 placeholder="Add a comment..."
-                className="w-80 md:w-96 h-auto p-2 bg-transparent"
+                className="w-60 md:w-80 h-auto p-2 bg-transparent border border-[#F8FAE5] rounded-lg"
               />
               <div className="pt-1 flex">
-                <button
-                  type="submit"
-                  className="px-4 py-2 bg-[#0f1d2c] text-white rounded-md"
-                >
+                <button type="submit" className="px-4 py-2 bg-[#0f1d2c] text-white rounded-md">
                   Comment
                 </button>
               </div>

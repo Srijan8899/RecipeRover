43ccd155095cc2ea4d35c256ea1cac2722ddbf25
Shreyansh Vyas <shreyanshv045@gmail.com>
2024-07-10 23:38:18 +0530

    updation in dependecies

--- front-end/src/pages/LandingPage.jsx
+++ front-end/src/pages/LandingPage.jsx
@@ -4,8 +4,10 @@ import Marquee from '../components/LandinPage/Marquee';
 import Explore from '../components/LandinPage/Explore';
 import Dining from '../components/LandinPage/Dining';
 import JoinUs from '../components/LandinPage/JoinUs';
+import LocomotiveScroll from 'locomotive-scroll'
 
 function LandingPage() {
+  const locomotiveScroll = new LocomotiveScroll();
   return (
   <div className="min-h-screen bg-black">
     <Hero/>
